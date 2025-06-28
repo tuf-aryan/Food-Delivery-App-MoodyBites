e89657d54fd03418cb34ca747e4041b86c9bd2d4
Aryan Gupta <aryangupta56240@gmail.com>
2025-06-29 00:28:25 +0530

    shimmer update

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,13 +3,13 @@ const RestaurantCard = (props) => {
   const { resData } = props;
   const { name, cuisines, avgRating, costForTwo } = resData?.info;
   return (
-    <div className=" w-72 h-86 sm:w-56 sm:h-72 m-4 border border-gray-200 shadow-lg">
+    <div className=" w-72 h-86 sm:w-56 sm:h-72 m-4 border border-gray-200 shadow-lg  hover:shadow-xl hover:bg-gray-100">
     <div>
       <img
-        className="w-72 h-56 sm:w-56 sm:h-44 p-4"
+        className="w-72 h-56 sm:w-full sm:h-44 p-4 rounded-lg"
         src={CDN_URL+resData?.info?.cloudinaryImageId}>
       </img>
-      <h3 className="font-bold py-2 text-s mx-3 truncate">{name}</h3>
+      <h3 className="font-bold text-gray-700 py-2 text-s mx-3 truncate">{name}</h3>
       <h5 className="truncate mx-3 text-xs">{cuisines?.join(",")}</h5>
       </div>
       <div className="flex justify-between text-xs m-3 items-center">
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,23 +1,13 @@
 const Shimmer = () => {
   return (
-    <div className="sfd flex flex-wrap ml-8">
-      
-      
-        <input className="w-56 h-8 mt-10 rounded-l-lg  bg-gray-100 ml-8"></input>
-        <button className="w-28 h-8 mt-10 bg-gray-100 rounded-r-lg "></button>
     
-
-      <div className="search m-2 p-3 flex items-center">
-        <button className="w-44 bg-gray-100 h-8 rounded-lg"></button>
-
-        <div className="search m-2 p-3 flex items-center">
-          <label className=" bg-gray-100 text-white w-36 h-8 p-1 rounded-l-lg">
-          </label>
-          <input className=" bg-gray-100 p-1 rounded-r-lg"></input>
-        </div>
-      </div>
+    <div className=" flex flex-wrap ml-8">
+    <div className="flex justify-center w-full m-4">
+      <input className="w-68 sm:w-96 mx-0 h-12 p-2 rounded-l-lg bg-gray-100"></input>
+      <button className="w-28 sm:w-28 bg-gray-100 h-12 rounded-r-lg">
+      </button>
+    </div>
       <div className="flex flex-wrap gap-8 mx-16">
-
         <div className="m-4 p-4 w-52 h-56  bg-gray-100  hover:bg-gray-200"></div>
         <div className="m-4 p-4 w-52 h-56 bg-gray-100  hover:bg-gray-200"></div>
         <div className="m-4 p-4 w-52 h-56 bg-gray-100  hover:bg-gray-200"></div>
@@ -27,7 +17,7 @@ const Shimmer = () => {
         <div className="m-4 p-4 w-52 h-56 bg-gray-100  hover:bg-gray-200"></div>
         <div className="m-4 p-4 w-52 h-56 bg-gray-100 hover:bg-gray-200"></div>
         <div className="m-4 p-4 w-52 h-56 bg-gray-100  hover:bg-gray-200"></div>
-        <div className="m-4 p-4 w-52 h-56 bg-gray-100 hover:bg-gray-200"></div>
+        <div className="m-4 p-4 w-52 h-56 bg-gray-100 rounded-lg hover:bg-gray-200"></div>
       </div>
     </div>
   );
